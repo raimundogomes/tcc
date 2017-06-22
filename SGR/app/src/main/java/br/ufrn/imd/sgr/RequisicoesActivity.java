f92691300b35a14f379236015fefa8b8f6981927
raimundo gomes <raimundo gomes>
2017-06-22 01:27:05 -0300

    Adicionando menu da tela principal

--- SGR/app/src/main/java/br/ufrn/imd/sgr/RequisicoesActivity.java
+++ SGR/app/src/main/java/br/ufrn/imd/sgr/RequisicoesActivity.java
@@ -1,11 +1,16 @@
 package br.ufrn.imd.sgr;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import br.ufrn.imd.sgr.utils.Constantes;
 
 public class RequisicoesActivity extends AppCompatActivity {
 
@@ -25,4 +30,51 @@ public class RequisicoesActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_sair:
+                finish();
+                break;
+            case R.id.menu_sincronizar:
+              //  sicronizarRequisicoes(requisicoes);
+                break;
+            case R.id.menu_configuracoes:
+            //    abrirConfiguracoes();
+                break;
+            case R.id.menu_desconectar:
+                desconectar();
+                break;
+            case R.id.menu_nova_requisicao:
+              //  novaRequisicao();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void desconectar() {
+        SharedPreferences sp = getSharedPreferences(Constantes.PREF_NAME, MODE_PRIVATE);
+
+        boolean b = sp.getBoolean(Constantes.CONFIGURACAO_CONECTADO, false);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.clear().commit();
+
+        finish();
+    }
+
 }
