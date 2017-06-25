128fc5cc3b343fc61bd3ad4c7ff04fc3588ed27c
raimundo gomes <raimundo gomes>
2017-06-25 03:21:59 -0300

    e Please enter the commit message for your changes. Lines starting

---
+++ SGR/app/src/main/java/br/ufrn/imd/sgr/utils/VolleyApplication.java
@@ -0,0 +1,32 @@
+package br.ufrn.imd.sgr.utils;
+
+/**
+ * Created by netou on 25/06/2017.
+ */
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyApplication extends Application {
+    private static VolleyApplication sInstance;
+
+    private RequestQueue mRequestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        sInstance = this;
+    }
+
+    public synchronized static VolleyApplication getInstance() {
+        return sInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return mRequestQueue;
+    }
+}
