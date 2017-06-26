7887282bd4958ab39693eb066ad667d2ec6e61cd
raimundo gomes <raimundo gomes>
2017-06-25 22:48:51 -0300

    adicionando tipo da coleta e material

---
+++ SGR/app/src/main/java/br/ufrn/imd/sgr/model/TipoColeta.java
@@ -0,0 +1,42 @@
+package br.ufrn.imd.sgr.model;
+
+
+public enum TipoColeta {
+
+    VEIA (0, "Veia periférica"),
+    ARTERIA   (1, "Arteria"),
+    CATETER_UMBILICAL (2, "Cateter umbilical"),
+    CATETER_CENTRAL (3, "Cateter central"),
+    JATO_MEDIO (4, "Jato médio"),
+    SVD   (5, "SVD"),
+    SONDA_ALIVIO   (6, "Sonda alívio"),
+    PUNCAO_SUBPUBICA (7, "Punção subpúbica"),
+    SACO_COLETOR (8, "Saco coletor"),
+    SWAB (8, "SWAB"),
+    ASPIRADO_AGULHA (8, "SWAB");
+
+    private int codigo;
+
+    private String descricao;
+
+    TipoColeta(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ SGR/app/src/main/java/br/ufrn/imd/sgr/model/TipoMaterial.java
@@ -0,0 +1,38 @@
+package br.ufrn.imd.sgr.model;
+
+
+public enum TipoMaterial {
+
+    FERIDA_OPERATORIA (0, "Ferida operatória"),
+    ABSCESSO   (1, "Abscesso"),
+    ULCERA   (2, "Úlcera"),
+    FRAGMENTO_TECIDO (3, "Fragmento de tecido"),
+    URINA(4, "Urina"),
+    SANGUE(5, "Sangue");
+
+
+    private int codigo;
+
+    private String descricao;
+
+    TipoMaterial(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
